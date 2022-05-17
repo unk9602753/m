38be4e55cda665fcdf5c81bbdc09c4b69927bfe5
unk9602753 <n.kuleshruslan@gmail.com>
2022-05-17 12:13:57 +0300

    commit 6

---
+++ api/src/main/java/com/epam/esm/controller/ExceptionHandlerController.java
@@ -0,0 +1,78 @@
+package com.epam.esm.controller;
+
+import com.epam.esm.entity.ResponseException;
+import com.epam.esm.config.Translator;
+import com.epam.esm.exception.ServiceException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(InvalidDefinitionException.class)
+    public ResponseEntity<ResponseException> handleInvalidDefinitionException(InvalidDefinitionException e) {
+        ResponseException re = errorBody(e,Translator.toLocale("exception.time.in.body"),HttpServletResponse.SC_BAD_REQUEST);
+        return new ResponseEntity<>(re,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(JsonParseException.class)
+    public ResponseEntity<ResponseException> handleJsonParseException(JsonParseException e) {
+        ResponseException re = errorBody(e,Translator.toLocale("exception.time.in.body"),HttpServletResponse.SC_BAD_REQUEST);
+        return new ResponseEntity<>(re,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<ResponseException> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e) {
+        ResponseException re = errorBody(e,Translator.toLocale("exception.column.null"),HttpServletResponse.SC_BAD_REQUEST);
+        return new ResponseEntity<>(re,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadSqlGrammarException.class)
+    public ResponseEntity<ResponseException> handleBadSqlGrammarException(BadSqlGrammarException e) {
+        ResponseException re = errorBody(e,e.getMessage(),HttpServletResponse.SC_BAD_REQUEST);
+        return new ResponseEntity<>(re,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<ResponseException> handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
+        ResponseException re = errorBody(e,e.getMessage(),HttpServletResponse.SC_NOT_FOUND);
+        return new ResponseEntity<>(re,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ResponseException> handleServiceException(ServiceException e) {
+        ResponseException re = errorBody(e,e.getMessage(),HttpServletResponse.SC_BAD_REQUEST, e.getId(),e.getConcreteMessage());
+        return new ResponseEntity<>(re,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ResponseException> handleNoSuchElementException(NoSuchElementException e) {
+        ResponseException re = errorBody(e,Translator.toLocale("exception.no.values.present"),HttpServletResponse.SC_BAD_REQUEST);
+        return new ResponseEntity<>(re,HttpStatus.BAD_REQUEST);
+    }
+
+    private String getErrorCodeFromClass(Throwable e) {
+        String className = Arrays.stream(e.getStackTrace()).findFirst().get().getClassName();
+        String temp = className.contains("Certificate") || className.contains("Tag") ? "01" : "03";
+        return temp.equals("01") && className.contains("Tag") ? "02" : temp;
+    }
+
+    private ResponseException errorBody(Throwable e, String messageCode, int code, String ... args){
+        ResponseException re = new ResponseException();
+        re.setCode(code + getErrorCodeFromClass(e));
+        re.setMessage(String.format(Translator.toLocale(messageCode),args));
+        return re;
+    }
+}
